c6d637be583f1591fb3bbd80766cffffc2073de0
verdantpath <m_dittmann@yahoo.com>
2018-07-22 22:59:12 -0700

    update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,27 @@
 $(document).ready(function() {
   var headclix = 0;
+  var eyeclix = 0;
+  var noseclix = 0;
+  var mouthclix = 0;
   $('#head').click(function() {
     if (headclix < 9) {
       headclix += 1;
     } else {
       headclix = 0;
+    }
+  });
+  $('#eyes').click(function() {
+    if (eyeclix < 9) {
+      eyeclix += 1;
+    } else {
+      eyeclix = 0;
+    }
+  });
+  $('#nose').click(function() {
+    if (noseclix < 9) {
+      noseclix += 1;
+    } else {
+      noseclix = 0;
     }
   });
 });
