8d644637e49c4b56ef66444ab4880a2ec81cf0cc
verdantpath <m_dittmann@yahoo.com>
2018-08-14 21:52:43 -0700

    update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,48 +5,48 @@ $(document).ready(function() {
   var noseclix = 0;
   var mouthclix = 0;
 
-  lightning_one();
-  lightning_two();
-  lightning_three();
+  // lightning_one();
+  // lightning_two();
+  // lightning_three();
 
-  $('#head').click(function() {
-    if (headclix < 9) {
-      headclix += 1;
-    } else {
-      headclix = 0;
-    }
-  });
-
-  $('#eyes').click(function() {
-    if (eyeclix < 9) {
-      eyeclix += 1;
-    } else {
-      eyeclix = 0;
-    }
-  });
-
-  $('#nose').click(function() {
-    if (noseclix < 9) {
-      noseclix += 1;
-    } else {
-      noseclix = 0;
-    }
-  });
-
-  $('#mouth').click(function() {
-    if (noseclix < 9) {
-      noseclix += 1;
-    } else {
-      noseclix = 0;
-    }
-  });
-
+  // $('#head').click(function() {
+  //   if (headclix < 9) {
+  //     headclix += 1;
+  //   } else {
+  //     headclix = 0;
+  //   }
+  // });
+  //
+  // $('#eyes').click(function() {
+  //   if (eyeclix < 9) {
+  //     eyeclix += 1;
+  //   } else {
+  //     eyeclix = 0;
+  //   }
+  // });
+  //
+  // $('#nose').click(function() {
+  //   if (noseclix < 9) {
+  //     noseclix += 1;
+  //   } else {
+  //     noseclix = 0;
+  //   }
+  // });
+  //
+  // $('#mouth').click(function() {
+  //   if (noseclix < 9) {
+  //     noseclix += 1;
+  //   } else {
+  //     noseclix = 0;
+  //   }
+  // });
+  //
   $('#head').click(function() {
     if (headclix < 9) {
       $(this).animate({left: "-=367px"}, 500),
       headclix += 1;
     } else {
-      $(this).anmiate({left: "0px"}, 500)
+      $(this).animate({left: "0px"}, 500)
       headclix = 0;
     }
   });
@@ -58,6 +58,25 @@ $(document).ready(function() {
     } else {
       $(this).animate({left: "0px"}, 500);
       eyeclix = 0;
+    }
+  });
+
+  $("#nose").click(function() {
+    if (noseclix < 9) {
+      $(this).animate({left: "-=367px"}, 500);
+      noseclix += 1;
+    } else {
+      $(this).animate({left: "0px"}, 500);
+      noseclix = 0;
+    }
+  });
+
+  $("#mouth").click(function() {
+    if (mouthclix < 9) {
+      $(this).animate({left: "-=367px"}, 500);
+      mouthclix += 1;
+    } else {
+      $(this).animate({left: "0px"}, 500);
     }
   });
 
@@ -77,6 +96,3 @@ function lightning_three() {
   $('#container #lightning3').fadeIn(250).fadeOut(250);
   setTimeout('lightning_three()', 8000);
 };
-
-$('#head').animate({left:"-=367px"}, 500);
-$('#head').animate({left:"0px"}, 500);
