b5922765f6be2bde031087b425b208015a48b36e
verdantpath <m_dittmann@yahoo.com>
2018-11-23 17:48:00 -0800

    update js

--- scripts.js
+++ scripts.js
@@ -9,45 +9,45 @@ $(function() {
   lightning_two();
   lightning_three();
 
-  $('#head').click(function() {
-    if (headclix < 9) {
-      $(this).animate({left: "-=367px"}, 500),
-      headclix += 1;
-    } else {
-      $(this).animate({left: "0px"}, 500)
-      headclix = 0;
-    }
-  });
-
-  $("#eyes").click(function() {
-    if (eyeclix < 9) {
-      $(this).animate({left: "-=367px"}, 500);
-      eyeclix += 1;
-    } else {
-      $(this).animate({left: "0px"}, 500);
-      eyeclix = 0;
-    }
-  });
-
-  $("#nose").click(function() {
-    if (noseclix < 9) {
-      $(this).animate({left: "-=367px"}, 500);
-      noseclix += 1;
-    } else {
-      $(this).animate({left: "0px"}, 500);
-      noseclix = 0;
-    }
-  });
-
-  $("#mouth").click(function() {
-    if (mouthclix < 9) {
-      $(this).animate({left: "-=367px"}, 500);
-      mouthclix += 1;
-    } else {
-      $(this).animate({left: "0px"}, 500);
-      mouthclix = 0;
-    }
-  });
+  // $('#head').click(function() {
+  //   if (headclix < 9) {
+  //     $(this).animate({left: "-=367px"}, 500),
+  //     headclix += 1;
+  //   } else {
+  //     $(this).animate({left: "0px"}, 500)
+  //     headclix = 0;
+  //   }
+  // });
+  //
+  // $("#eyes").click(function() {
+  //   if (eyeclix < 9) {
+  //     $(this).animate({left: "-=367px"}, 500);
+  //     eyeclix += 1;
+  //   } else {
+  //     $(this).animate({left: "0px"}, 500);
+  //     eyeclix = 0;
+  //   }
+  // });
+  //
+  // $("#nose").click(function() {
+  //   if (noseclix < 9) {
+  //     $(this).animate({left: "-=367px"}, 500);
+  //     noseclix += 1;
+  //   } else {
+  //     $(this).animate({left: "0px"}, 500);
+  //     noseclix = 0;
+  //   }
+  // });
+  //
+  // $("#mouth").click(function() {
+  //   if (mouthclix < 9) {
+  //     $(this).animate({left: "-=367px"}, 500);
+  //     mouthclix += 1;
+  //   } else {
+  //     $(this).animate({left: "0px"}, 500);
+  //     mouthclix = 0;
+  //   }
+  // });
 
 });
 
